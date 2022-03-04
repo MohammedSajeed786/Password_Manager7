b26871a0be6695c8543e9ec44a3e7c9d2eaa1b89
Sajeed <mdsajeedmds@gmail.com>
2022-03-04 14:46:39 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,22 @@
+const connectToMongo = require("./db.js");
+const express = require("express");
+connectToMongo();
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+var cors = require("cors");
+app.use(cors());
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/passwords", require("./routes/passwords"));
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("frontend/build"));
+}
+
+app.listen(port, () => {
+  console.log(`Password Manager listening on port ${port}`);
+});
+//5689nk2022
